f43fe9c02810fdbad63836557b1cfef0aef877c9
morganottosson <morganottosson@outlook.com>
2018-12-13 14:01:54 +0100

    removed files

--- source/js/store/demo/types.js
+++
@@ -1,6 +0,0 @@
-export var Constants;
-(function (Constants) {
-    Constants["ADD_ITEM"] = "ADD_ITEM";
-    Constants["SET_LOADING"] = "SET_LOADING";
-})(Constants || (Constants = {}));
-//# sourceMappingURL=types.js.map
