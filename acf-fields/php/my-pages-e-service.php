cdf28af8337de5c45d76034b0acfd1ad07ca250e
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2018-12-13 14:32:44 +0100

    Adds field to enable the administrator to provide a simple description about this service.

--- acf-fields/php/my-pages-e-service.php
+++ acf-fields/php/my-pages-e-service.php
@@ -4,7 +4,27 @@
     acf_add_local_field_group(array(
     'key' => 'group_5c125d8781f32',
     'title' => __('E-Service Form Input', 'my-pages'),
-    'fields' => false,
+    'fields' => array(
+        0 => array(
+            'key' => 'field_5c125efc8254f',
+            'label' => __('Desciption', 'my-pages'),
+            'name' => 'service_description',
+            'type' => 'textarea',
+            'instructions' => __('A short description about the service. What\'s it about? Whats the end-user goal?', 'my-pages'),
+            'required' => 1,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+            'maxlength' => 500,
+            'rows' => 8,
+            'new_lines' => '',
+        ),
+    ),
     'location' => array(
         0 => array(
             0 => array(
