675eba2d6d60384ff8336ea2213bf4f1e5b577e5
Eldin Behlic <eldin.behlic@hiq.se>
2019-01-09 14:24:48 +0100

    gitignore fix for /built folder

--- built/store/person/types.js
+++
@@ -1,8 +0,0 @@
-export var Constants;
-(function (Constants) {
-    // SENDFORM = 'SENDFORM',
-    Constants["LOGGED_SUCCESSFULLY"] = "LOGGED_SUCCESSFULLY";
-    Constants["LOGGED_FAILED"] = "LOGGED_FAILED";
-    Constants["LOGIN_ATTEMPT"] = "LOGIN_ATTEMPT";
-})(Constants || (Constants = {}));
-//# sourceMappingURL=types.js.map
--- built/store/user/types.js
+++
@@ -1,7 +0,0 @@
-export var Const;
-(function (Const) {
-    Const["AUTH_FAIL"] = "AUTHENTICATION_FAILURE";
-    Const["AUTH_SUCCESS"] = "AUTHENTICATION_SUCCESS";
-    Const["AUTH_LOGOUT"] = "AUTHENTICATION_LOGOUT";
-})(Const || (Const = {}));
-//# sourceMappingURL=types.js.map
