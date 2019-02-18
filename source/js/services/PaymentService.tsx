6aba0d17e10ce097c50bc149be93fc1a0732f2fd
Eldin Behlic <eldin.behlic@hiq.se>
2019-02-18 13:24:59 +0100

    - The orderdata is now fetched via the api on load of the confirmation page and the order status is updated.

--- source/js/services/PaymentService.tsx
+++ source/js/services/PaymentService.tsx
@@ -1,10 +1,17 @@
 import { AxiosResponse } from "axios";
-import { post } from "./Requests";
+import { post, get } from "./Requests";
 
 export interface IOrderCreate {
     totalAmount: number;
 }
 
+export interface IConfirmOrderResponse {
+    OrderId: number;
+    TotalAmount: number;
+    Status: number;
+    UserId: number;
+    ExternalOrderId: string;
+}
 export const initializePayment = async (
     parameters: IOrderCreate
 ): Promise<string> => {
@@ -28,4 +35,25 @@ export const initializePayment = async (
 
     console.log(result);
     return result.paymentInfo.url;
+};
+
+export const confirmPayment = async (
+    orderId: string
+): Promise<IConfirmOrderResponse> => {
+    const host = process.env.API_URL;
+    const endpoint = `/payment/confirm/${orderId}`;
+
+    const result = await get(`${host}${endpoint}`).then(
+        (response: AxiosResponse<IConfirmOrderResponse>) => {
+            if (response.status !== 200) {
+                console.log(response.data);
+                return null;
+            } else {
+                return response.data;
+            }
+        }
+    );
+
+    console.log(result);
+    return result;
 };
